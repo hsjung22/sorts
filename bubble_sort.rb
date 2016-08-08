0b53760a32130ec8f4109111142a1a8432e6c00b
hsjung22 <hsjung22@gmail.com>
2016-08-07 19:08:58 -0700

    ruby practice

---
+++ bubble_sort.rb
@@ -0,0 +1,30 @@
+def bubble_sort(arr)
+
+	did_sort = true
+	while did_sort
+		did_sort = false
+
+		arr.length.times do |index|
+			if index < arr.length-1 && arr[index] > arr[index+1]
+				 temp = arr[index]
+				 arr[index] = arr[index+1]
+				 arr[index+1] = temp
+				 did_sort = true
+			end
+		end
+
+
+	end
+	arr
+end
+
+
+
+
+arr = Array.new(10) { rand(1..99) }
+
+
+puts "before #{arr}"
+bubble_sort(arr)
+puts "after #{arr}"
+
